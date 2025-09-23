5bea27eb30c708ba4c16f8624edd22d7c9d26e31
joelscreen <165317690+joelscreen@users.noreply.github.com>
2025-09-23 23:57:14 +0400

    Update main.py

--- projects/PBL 2025/main.py
+++ projects/PBL 2025/main.py
@@ -8,10 +8,20 @@ app = ctk.CTk()
 app.title("PBL 2025")
 app.geometry("400x300")
 
+screen_w = app.winfo_screenwidth()
+screen_h = app.winfo_screenheight()
+
 gui = Image.open("assets/images/gui.png")
-gui = gui.resize((app.winfo_screenmmwidth(), app.winfo_screenmmheight()))
-gui_photo = ctk.CTkImage(gui)
+gui_photo = ctk.CTkImage(dark_image=gui, size=(screen_w, screen_h))
 gui_label = ctk.CTkLabel(app, image=gui_photo, text="")
 gui_label.place(relx=0, rely=0)
+
+def full_screen(event=None):
+    app.attributes("-fullscreen", True)
+def exit_fullscreen(event=None):
+    app.attributes("-fullscreen", False)
+
+app.bind("<F11>", full_screen)
+app.bind("<Escape>", exit_fullscreen)
 
 app.mainloop()
